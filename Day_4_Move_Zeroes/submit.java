a6e89039193ef855bbf8c7d49adc196d340ac3eb
AndrejLee <ledinhhan00@gmail.com>
2020-04-05 21:56:27 +0700

    Update submit.java

--- Day_4_Move_Zeroes/submit.java
+++ Day_4_Move_Zeroes/submit.java
@@ -2,25 +2,15 @@ https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-
 
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length <= 1)
-            return;
-        int iterator = 0;
-        while (iterator < nums.length && nums[iterator] != 0) {
-            ++iterator;
-        }
-        int nonZeroIndex = iterator++;
-        while (iterator < nums.length) {
-            if (nums[iterator] != 0) {
-                swap(nums, nonZeroIndex, iterator);
-                ++nonZeroIndex;
+        int nonZeroIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
             }
-            ++iterator;
         }
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        nums[i] = nums[i] ^ nums[j];
-        nums[j] = nums[i] ^ nums[j];
-        nums[i] = nums[i] ^ nums[j];
+        
+        for (int i = nonZeroIndex; i < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 }
